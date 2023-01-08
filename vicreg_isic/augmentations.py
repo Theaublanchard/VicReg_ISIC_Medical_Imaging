ISIC ready with LARS+scaler and fixed precision. Not distributed

vicreg_isic/augmentations.py
@@ -41,6 +41,7 @@ class Solarization(object):
             return ImageOps.solarize(img)
         else:
             return img
+            
 isic_mean = torch.tensor([0.6116, 0.4709, 0.4692])
 isic_std = torch.tensor([0.2560, 0.2217, 0.2273])
 
@@ -59,7 +60,7 @@ class TrainTransform(object):
                 transforms.RandomRotation(360),
                 GaussianBlur(p=1.),
                 transforms.RandomApply([
-                    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.)
+                    transforms.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.2, hue=0.)
                     ], p=0.8),
                 transforms.ToTensor(),
                 transforms.Normalize(isic_mean, isic_std),
@@ -77,65 +78,11 @@ class TrainTransform(object):
                 transforms.RandomRotation(360),
                 GaussianBlur(p=0.1),
                 transforms.RandomApply([
-                    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.)
+                    transforms.ColorJitter(brightness=0.1, contrast=0.2, saturation=0.1, hue=0.)
                     ], p=0.8),
                 transforms.ToTensor(),
                 transforms.Normalize(isic_mean, isic_std)   
             ])  
-        self.transform = transforms.Compose(
-            [
-                # transforms.ToTensor(),
-                # transforms.ConvertImageDtype(torch.float32),
-                # transforms.RandomResizedCrop(
-                #     224, interpolation=InterpolationMode.BICUBIC
-                # ),
-                # transforms.ToPILImage(),
-                transforms.RandomHorizontalFlip(p=0.5),
-                transforms.RandomApply(
-                    [
-                        transforms.ColorJitter(
-                            brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1
-                        )
-                    ],
-                    p=0.8,
-                ),
-                transforms.RandomGrayscale(p=0.2),
-                GaussianBlur(p=.5),
-                Solarization(p=0.0),
-                transforms.ToTensor(),
-                transforms.Normalize(
-                    # mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225] #imageNet
-                    mean=[0.49139968, 0.48215841, 0.44653091], std=[0.24703223, 0.24348513, 0.26158784] #CIFAR10#
-                ),
-            ]
-        )
-        self.transform_prime = transforms.Compose(
-            [
-                # transforms.ToTensor(),
-                # transforms.ConvertImageDtype(torch.float32),
-                # transforms.RandomResizedCrop(
-                #     224, interpolation=InterpolationMode.BICUBIC
-                # ),
-                # transforms.ToPILImage(),
-                transforms.RandomHorizontalFlip(p=0.5),
-                transforms.RandomApply(
-                    [
-                        transforms.ColorJitter(
-                            brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1
-                        )
-                    ],
-                    p=0.8,
-                ),
-                transforms.RandomGrayscale(p=0.2),
-                GaussianBlur(p=0.1),
-                Solarization(p=0.2),
-                transforms.ToTensor(),
-                transforms.Normalize(
-                    # mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225] #imageNet
-                    mean=[0.49139968, 0.48215841, 0.44653091], std=[0.24703223, 0.24348513, 0.26158784] #CIFAR10
-                ),
-            ]
-        )
 
     def __call__(self, sample):
         x1 = self.transform(sample)

vicreg_isic/evaluate.py
@@ -23,12 +23,12 @@ import torch
 from tqdm import tqdm
 
 import resnet
-import numpy as np
+import augmentations
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(
-        description="Evaluate a pretrained model on ImageNet"
+        description="Evaluate a pretrained model on ISIC 2019"
     )
 
     # Data
@@ -191,10 +191,25 @@ def main_worker(args):
         best_acc = argparse.Namespace(top1=0, top5=0)
 
     # Data loading code
-    # traindir = args.data_dir / f"train_{args.train_percent}"
-    # valdir = args.data_dir / "test"
+    traindir = args.data_dir / f"train_{args.train_percent}"
+    valdir = args.data_dir / "test"
     normalize = transforms.Normalize(
         mean=[0.49139968, 0.48215841, 0.44653091], std=[0.24703223, 0.24348513, 0.26158784]
+    )
+
+    train_dataset = datasets.ImageFolder(
+        traindir,
+        transforms.Compose(
+            [
+                transforms.ToTensor(),
+                augmentations.CropMainAxis(),
+                transforms.Resize((224, 224)),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+                transforms.ToTensor(),
+                normalize,
+            ]
+        ),
     )
 
     train_dataset_100 = datasets.CIFAR10(root= args.data_dir,train=True,download=True,transform=transforms.Compose(
@@ -209,8 +224,8 @@ def main_worker(args):
         )
     )
 
-    idx_to_keep = np.random.choice(len(train_dataset_100), int(len(train_dataset_100)*args.train_percent/100), replace=False)
-    train_dataset = torch.utils.data.Subset(train_dataset_100, idx_to_keep)
+    # idx_to_keep = np.random.choice(len(train_dataset_100), int(len(train_dataset_100)*args.train_percent/100), replace=False)
+    # train_dataset = torch.utils.data.Subset(train_dataset_100, idx_to_keep)
 
     val_dataset = datasets.CIFAR10(root= args.data_dir,train=False,download=True,transform= transforms.Compose(
             [

vicreg_isic/main_vicreg.py
@@ -97,7 +97,7 @@ def main(args):
 
     transforms = aug.TrainTransform()
 
-    dataset = datasets.ImageFolder(args.data_dir / "train_100", transforms,shuffle=True)
+    dataset = datasets.ImageFolder(args.data_dir / "train_100", transforms)
     # sampler = torch.utils.data.distributed.DistributedSampler(dataset, shuffle=True)
     assert args.batch_size % args.world_size == 0
     per_device_batch_size = args.batch_size // args.world_size
@@ -113,15 +113,15 @@ def main(args):
     model = VICReg(args).cuda(gpu)
     # model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
     # model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
-    # optimizer = LARS(
-    #     model.parameters(),
-    #     lr=0,
-    #     weight_decay=args.wd,
-    #     weight_decay_filter=exclude_bias_and_norm,
-    #     lars_adaptation_filter=exclude_bias_and_norm,
-    # )
+    optimizer = LARS(
+        model.parameters(),
+        lr=0,
+        weight_decay=args.wd,
+        weight_decay_filter=exclude_bias_and_norm,
+        lars_adaptation_filter=exclude_bias_and_norm,
+    )
     # print(model.backbone)
-    optimizer = optim.Adam(model.parameters(), lr=0, weight_decay=args.wd,)
+    # optimizer = optim.Adam(model.parameters(), lr=0, weight_decay=args.wd,)
 
     if (args.exp_dir / "model.pth").is_file():
         # if args.rank == 0:
@@ -148,8 +148,8 @@ def main(args):
             lr = adjust_learning_rate(args, optimizer, loader, step)
 
             optimizer.zero_grad()
-            with torch.cuda.amp.autocast():
-                loss = model.forward(x, y)
+            # with torch.cuda.amp.autocast():
+            loss = model.forward(x, y)
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()

vicreg_perso/main_vicreg.py
@@ -149,8 +149,8 @@ def main(args):
             lr = adjust_learning_rate(args, optimizer, loader, step)
 
             optimizer.zero_grad()
-            with torch.cuda.amp.autocast():
-                loss = model.forward(x, y)
+            # with torch.cuda.amp.autocast():
+            loss = model.forward(x, y)
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()